0e916ab6a108709988b0e3c64a30b7056d9302da
DimitrisBo <31184597+DimitrisBovasianos@users.noreply.github.com>
2019-01-07 17:01:53 +0200

    Update auth.py

--- frontend/auth.py
+++ frontend/auth.py
@@ -9,7 +9,7 @@ from django.conf import settings
 class MyAuthentication(object):
 
     def authenticate(self,request,username,password):
-        creds = {'client_id':'yD8oVDsbfip7yZLqqBeMGf1AYRwvRF2CzudzzqM9','client_secret':'Et427gqwkzMdNGiJKUa1UaChYBlbXffvmNBKaT0ue7eCjepai7D8rTltbwwcGLksIw5WikVLVVK9pOAvh8AwnNzRlzSOF2MTAXkku1qWeJUDDVke8XM7kt58C8OJMuOC','grant_type':'password','username':username,'password':password}
+        creds = {'client_id':'<cliend_id>','client_secret':'client_secret','grant_type':'password','username':username,'password':password}
         creds = parse.urlencode(creds).encode()
         url = 'http://127.0.0.1:8000/o/token/'
         req = requestalter.Request(url,creds)
@@ -20,7 +20,7 @@ class MyAuthentication(object):
         token = data['access_token']
         if token:
             string = 'Bearer {}'.format(token)
-            request.session.set_expiry(20)
+            request.session.set_expiry(data['expires_in])
             request.session['refresh_token'] = data['refresh_token']
             user = MyUser.objects.get(username=username)
             return user
@@ -35,11 +35,3 @@ class MyAuthentication(object):
     def has_perm(self, user_obj, perm, obj=None):
         return user_obj.username == settings.ADMIN_LOGIN
 
-class MyAuthenticationRest(BaseAuthentication):
-
-    def authenticate(self,request):
-        user = request.user
-        if user:
-            return (user,None)
-        else:
-            return None
