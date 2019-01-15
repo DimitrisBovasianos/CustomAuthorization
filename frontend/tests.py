c55682232b2dd5e10f0d2ff401b6608963ecff0e
DimitrisBo <31184597+DimitrisBovasianos@users.noreply.github.com>
2019-01-15 12:47:16 +0200

    Update tests.py

--- frontend/tests.py
+++ frontend/tests.py
@@ -1,3 +1,22 @@
 from django.test import TestCase
+from rest_framework.test import APIRequestFactory,APITestCase
+from .models import MyUser
+from .views import *
+from rest_framework.test import force_authenticate
 
-# Create your tests here.
+class Cases(APITestCase):
+  
+    def setUp(self):
+        self.user = MyUser.objects.create_user(username='dimi',email='thahe@gmail.com')
+        self.user.set_password('king2306')
+        self.user.save()
+
+
+
+    def test_login(self):
+        view = Home.as_view()
+        factory = APIRequestFactory()
+        request = factory.get('/')
+        force_authenticate(request,self.user)
+        response = view(request)
+        self.assertEqual(response.status_code, 200)
